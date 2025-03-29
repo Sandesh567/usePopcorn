b754161d5ab9841e4f21da9bb645d23462b8ab30
sandesh <sandeshbhatta218@gmail.com>
2025-03-29 12:43:38 +0545

    Adding Watched movie list name.

--- src/App.js
+++ src/App.js
@@ -364,3 +364,16 @@ function WatchedSummary({ watched }) {
   );
 }
 
+function WatchedMoviesList({ watched, onDeleteWatched }) {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie
+          movie={movie}
+          key={movie.imdbID}
+          onDeleteWatched={onDeleteWatched}
+        />
+      ))}
+    </ul>
+  );
+}
