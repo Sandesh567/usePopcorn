974e2cb240dec91aaa16da03a6d04bd80570a5bc
sandesh <sandeshbhatta218@gmail.com>
2025-04-05 12:26:01 +0545

    Search Query function is added.

--- src/App-v3.js
+++ src/App-v3.js
@@ -155,3 +155,43 @@ function Logo() {
         </div>
     );
 }
+
+
+
+function Search({ query, setQuery }) {
+    const inputEl = useRef(null);
+
+    useEffect(
+        function () {
+            function callback(e) {
+                if (document.activeElement === inputEl.current) return;
+
+                if (e.code === "Enter") {
+                    inputEl.current.focus();
+                    setQuery("");
+                }
+            }
+
+            document.addEventListener("keydown", callback);
+            return () => document.addEventListener("keydown", callback);
+        },
+        [setQuery]
+    );
+
+    // useEffect(function () {
+    //   const el = document.querySelector(".search");
+    //   console.log(el);
+    //   el.focus();
+    // }, []);
+
+    return (
+        <input
+            className="search"
+            type="text"
+            placeholder="Search movies..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            ref={inputEl}
+        />
+    );
+}
