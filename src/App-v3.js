a76013997a539b647b98a32fe97d69c6ddbdcdcc
sandesh <sandeshbhatta218@gmail.com>
2025-04-06 13:34:51 +0545

    Creating MovieList for list of movies.

--- src/App-v3.js
+++ src/App-v3.js
@@ -226,3 +226,14 @@ function Box({ children }) {
         </div>
     );
 }
+
+
+function MovieList({ movies, onSelectMovie }) {
+    return (
+        <ul className="list list-movies">
+            {movies?.map((movie) => (
+                <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
+            ))}
+        </ul>
+    );
+}
