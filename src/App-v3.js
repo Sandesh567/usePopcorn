a069b593e4dfd054f75e2fe3877bfce789493469
sandesh <sandeshbhatta218@gmail.com>
2025-04-09 19:24:11 +0545

    Using useEffect to fetch data and render.

--- src/App-v3.js
+++ src/App-v3.js
@@ -95,3 +95,91 @@ export default function App() {
     function handleDeleteWatched(id) {
         setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
     }
+
+    useEffect(
+        function () {
+            const controller = new AbortController();
+
+            async function fetchMovies() {
+                try {
+                    setIsLoading(true);
+                    setError("");
+
+                    const res = await fetch(
+                        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+                        { signal: controller.signal }
+                    );
+
+                    if (!res.ok)
+                        throw new Error("Something went wrong with fetching movies");
+
+                    const data = await res.json();
+                    if (data.Response === "False") throw new Error("Movie not found");
+
+                    setMovies(data.Search);
+                    setError("");
+                } catch (err) {
+                    if (err.name !== "AbortError") {
+                        console.log(err.message);
+                        setError(err.message);
+                    }
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+
+            if (query.length < 3) {
+                setMovies([]);
+                setError("");
+                return;
+            }
+
+            handleCloseMovie();
+            fetchMovies();
+
+            return function () {
+                controller.abort();
+            };
+        },
+        [query]
+    );
+
+    return (
+        <>
+            <NavBar>
+                <Search query={query} setQuery={setQuery} />
+                <NumResults movies={movies} />
+            </NavBar>
+
+            <Main>
+                <Box>
+                    {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
+                    {isLoading && <Loader />}
+                    {!isLoading && !error && (
+                        <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+                    )}
+                    {error && <ErrorMessage message={error} />}
+                </Box>
+
+                <Box>
+                    {selectedId ? (
+                        <MovieDetails
+                            selectedId={selectedId}
+                            onCloseMovie={handleCloseMovie}
+                            onAddWatched={handleAddWatched}
+                            watched={watched}
+                        />
+                    ) : (
+                        <>
+                            <WatchedSummary watched={watched} />
+                            <WatchedMoviesList
+                                watched={watched}
+                                onDeleteWatched={handleDeleteWatched}
+                            />
+                        </>
+                    )}
+                </Box>
+            </Main>
+        </>
+    );
+}
