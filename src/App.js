0074adae0244d36cb50a13a45f5220931571ec8a
sandesh <sandeshbhatta218@gmail.com>
2025-04-10 13:46:58 +0545

    Adding LocalStorage so the movie list is not removed when page is loaded.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
-// import StarRating from "./StarRating";
+import StarRating from "./StarRating";
 import { useKey } from "./useKey";
-// import { useLocalStorageState } from "./useLocalStorageState";
+import { useLocalStorageState } from "./useLocalStorageState";
 import { useMovies } from "./useMovies";
 
 const average = (arr) =>
@@ -15,7 +15,7 @@ export default function App() {
   const [selectedId, setSelectedId] = useState(null);
   const { movies, isLoading, error } = useMovies(query);
 
-  // const [watched, setWatched] = useLocalStorageState([], "watched");
+  const [watched, setWatched] = useLocalStorageState([], "watched");
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
